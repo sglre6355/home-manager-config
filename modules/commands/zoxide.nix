c6879d1347bdfbc7da88cb91783e16626a133d90
sglre6355 <sglre6355@gmail.com>
2026-05-23 19:56:47 +0100

    Exclude non-home paths from indexing

--- modules/commands/zoxide.nix
+++ modules/commands/zoxide.nix
@@ -2,6 +2,10 @@
   ...
 }:
 {
+  home.sessionVariables = {
+    _ZO_EXCLUDE_DIRS = "/:/h:/ho:/hom:/home:/home/:/[!h]*:/h[!o]*:/ho[!m]*:/hom[!e]*:/home[!/]*";
+  };
+
   programs.zoxide = {
     enable = true;
     options = [
