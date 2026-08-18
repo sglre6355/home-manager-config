0577920e872da7d0366617bee563bbf0e391eff9
sglre6355 <sglre6355@gmail.com>
2026-08-18 10:00:23 +0900

    Add hatena profile and enable it on SGR-PCPB01

--- hosts/sgr-pcpb01.nix
+++ hosts/sgr-pcpb01.nix
@@ -4,6 +4,7 @@
 {
   imports = [
     ../profiles/sglre6355.nix
+    ../profiles/hatena.nix
   ];
 
   home.stateVersion = "26.05";
---
+++ profiles/hatena.nix
@@ -0,0 +1,21 @@
+{
+  pkgs,
+  lib,
+  ...
+}:
+{
+  imports = [
+    ./core.nix
+
+    ../modules/development/claude-code.nix
+    ../modules/development/devenv.nix
+  ];
+
+  home.packages = with pkgs; [
+    slack
+    (lib.lowPrio minikube)
+    skaffold
+    go
+    nodejs
+  ];
+}
