2fdc3a6e4a792396b4c14223a4998d30e45c1fa8
sglre6355 <sglre6355@gmail.com>
2026-09-08 02:40:34 +0900

    Manage `herdr` config in Nix and bind `open_worktree`

--- modules/development/herdr.nix
+++ modules/development/herdr.nix
@@ -4,5 +4,25 @@
 {
   programs.herdr = {
     enable = true;
+    settings = {
+      onboarding = false;
+
+      theme = {
+        name = "rose-pine";
+        auto_switch = false;
+      };
+
+      ui = {
+        agent_panel_sort = "spaces";
+      };
+
+      keys = {
+        open_worktree = "prefix+shift+o";
+      };
+
+      experimental = {
+        pane_history = false;
+      };
+    };
   };
 }
